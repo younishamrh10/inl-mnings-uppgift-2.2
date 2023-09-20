94752442087c57ebeabb5c8d48580b0c970a74ea
younishamrh10 <yoha06001@utb.vaxjo.se>
2023-09-20 10:06:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uppgift_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin i längdhopp?");
+            float X = float.Parse(Console.ReadLine());
+            Console.WriteLine("Hu långt hoppade Alma i längdhopp?");
+            float Y = float.Parse(Console.ReadLine());
+            float Z = X - Y;
+            Console.WriteLine("Elin hoppade" + Z + "meter längre äm Alma");
+        }
+    }
+}
